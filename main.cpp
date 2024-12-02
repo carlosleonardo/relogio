a8a5bf1df46fda2270b3364085315f2fc0fc2c61
Carlos Leonardo Goes Farias <carlos.leonardo.goes@gmail.com>
2024-12-02 18:53:04 -0300

    Ajuste para usar a biblioteca padrão

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <chrono>
-#include <date/date.h>
-#include <date/tz.h>
 
 int main()
 {
@@ -9,10 +7,9 @@ int main()
     try
     {
         auto now = std::chrono::system_clock::now();
-        auto zone = date::locate_zone("America/Sao_Paulo");
-        auto zoned_time = date::make_zoned(zone, now);
-        auto local_time = zoned_time.get_local_time();
-        std::cout << date::format("%T", zoned_time) << '\n';
+        auto zone = std::chrono::zoned_time(std::chrono::current_zone(), now);
+
+        std::cout << std::format("{:%H:%M:%S}", zone) << '\n';
     }
     catch (const std::exception &e)
     {
