e0478545e810bfcb139595864bd5e8f23af87a4f
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2024-12-03 08:59:29 -0300

    Adaptado para GCC antes da versão 11, enquanto mantém a versão mais nova.

--- main.cpp
+++ main.cpp
@@ -11,10 +11,20 @@ int main()
         while (true)
         {
             auto now = std::chrono::system_clock::now();
+#if __GNUC__ > 11
             auto zone = std::chrono::zoned_time(std::chrono::current_zone(), now);
 
             const auto formatado = std::format("{:%H:%M:%S}", zone);
             fmt::print("\rHorário: {}", formatado);
+#else
+            auto time = std::chrono::system_clock::to_time_t(now);
+            auto local_time = *std::localtime(&time);
+
+            fmt::print("\rHorário: {:02d}:{:02d}:{:02d}",
+                       local_time.tm_hour,
+                       local_time.tm_min,
+                       local_time.tm_sec);
+#endif
             // std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
     }
