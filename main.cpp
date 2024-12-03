9c2327a5dbeadc95d66d722fe0916a2fb0cc8af1
Carlos Leonardo Goes Farias <carlosleonardogoes@outlook.com>
2024-12-02 21:43:34 -0300

    Colocado em loop até interromper

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <chrono>
 #include <fmt/format.h>
+#include <thread>
 
 int main()
 {
 
     try
     {
-        auto now = std::chrono::system_clock::now();
-        auto zone = std::chrono::zoned_time(std::chrono::current_zone(), now);
+        while (true)
+        {
+            auto now = std::chrono::system_clock::now();
+            auto zone = std::chrono::zoned_time(std::chrono::current_zone(), now);
 
-        const auto formatado = std::format("{:%H:%M:%S}", zone);
-        fmt::print("Horário: {}\n", formatado);
+            const auto formatado = std::format("{:%H:%M:%S}", zone);
+            fmt::print("\rHorário: {}", formatado);
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
     }
     catch (const std::exception &e)
     {
